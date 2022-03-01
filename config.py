ef5f3bad6a67ef749dc54ded4e9c3359fc276577
Ravi11ndu <100390033+Ravi11ndu@users.noreply.github.com>
2022-03-01 08:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("5271469949:AAF1K4-j2wCQNmgf2w2ts__y7S-ou5D8klw", "")
+    BOT_TOKEN = os.environ.get("5271448590:AAHz07JQwrUUx-vO2tE_C5Xvg8qVC9lThvE", "")
     # The Telegram API things
-    API_ID = int(os.environ.get("7322056",))
-    API_HASH = os.environ.get("89074aa74ed297150b3120914946db5c")
+    API_ID = int(os.environ.get("11391818",)
+    API_HASH = os.environ.get("ae243843b0c6fe3b59f0919d5d5a84f8")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -18,14 +18,14 @@ class Config(object):
     # default thumbnail to be used in the videos
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("182.74.243.47:3128", "")
+    HTTP_PROXY = os.environ.get("", "")
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("1387186514", ""))
+    OWNER_ID = int(os.environ.get("5131599131", ""))
     SESSION_NAME = "UPLOADER-RVX-BOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("postgres://lqgzvowgqawpig:63442795843431f67c93c37fba1e5963310b49f4173346576f1b72576f5879c2@ec2-34-230-110-100.compute-1.amazonaws.com:5432/d7qkolg9gvqf5i", "")
+    DATABASE_URL = os.environ.get("mongodb+srv://Ravindu:Ravi12ndu.@cluster0.y33ek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", "")
     MAX_RESULTS = "50"
